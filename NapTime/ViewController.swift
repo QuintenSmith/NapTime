effb65c575d57c0f32cc5549750aa7b809693350
QuintenSmith <quintensmith15@gmail.com>
2018-08-28 09:45:42 -0700

    Initial Commit

---
+++ NapTime/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NapTime
+//
+//  Created by Quinten Smith on 8/28/18.
+//  Copyright © 2018 Quinten Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
